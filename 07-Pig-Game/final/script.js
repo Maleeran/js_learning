ebbd2844297f631027efeb01f81b8ec08b12a656
Liangbo Gao <15803871761@163.com>
2026-01-05 22:20:04 +0800

    1.5 second save

--- 07-Pig-Game/final/script.js
+++ 07-Pig-Game/final/script.js
@@ -1,1 +1,75 @@
 'use strict';
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+};
+
+const toggleActivePlayerVisual = () => {
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+const checkWin = () => {
+  if (scores[activePlayer] >= 10) {
+    alert(`🎉 Player ${activePlayer + 1} wins!`);
+  }
+};
+
+// Rolling Dice Functionality
+btnRoll.addEventListener('click', () => {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    switchPlayer();
+    toggleActivePlayerVisual();
+  }
+});
+
+// Hold button
+btnHold.addEventListener('click', () => {
+  scores[activePlayer] += currentScore;
+  checkWin();
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  switchPlayer();
+  toggleActivePlayerVisual();
+  console.log(scores);
+});
+
+// init();
+// btnNew.addEventListener('click', init);
