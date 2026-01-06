66a803178dea94a970c57b99d9aea62c79a129e3
Liangbo Gao <15803871761@163.com>
2026-01-06 21:50:42 +0800

    1.6 save

---
+++ 08-Behind-the-Scenes/final/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function calcAge(birthYear) {
+  const age = 2026 - birthYear;
+  console.log(firstName);
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Maleeran';
+calcAge(2001);
