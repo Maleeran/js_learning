e2af45e691c34650326786857fa8a113653f2ceb
Liangbo Gao <15803871761@163.com>
2026-01-19 00:06:40 +0800

    1.18 second save

--- 14-OOP/final/script.js
+++ 14-OOP/final/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 const Person = function (firstName, age) {
@@ -600,7 +600,7 @@ let coffeeMachine = new CoffeeMachine(100);
 
 coffeeMachine.addWater(50).addMilk().makeCoffee();
 coffeeMachine.power = 25; // Error (no setter)
-*/
+
 
 class CoffeeMachine {
   _waterAmount = 0; // the amount of water inside
@@ -656,3 +656,71 @@ let coffeeMachine = new CoffeeMachine(100);
 coffeeMachine.addWater(50).addMilk().makeCoffee();
 
 coffeeMachine.clean().addWater(100).makeCoffee();
+*/
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+
+    return this;
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${this.#charge}`,
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl("Rivian", 120, 23);
+console.log(rivian);
+// console.log(rivian.#charge); // Error
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattesry(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
+*/
